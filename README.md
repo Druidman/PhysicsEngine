369efd12358e28aca37581f8127f58b26c6aa096
Bober <towermanxxl@gmail.com>
2026-09-14 20:00:03 +0200

    init commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# This repo is for physics simulation in opengl and c++
+Psst... **This will be base for ai robot sim**
---
+++ main.cpp
@@ -0,0 +1,56 @@
+#include "vendor/glad/glad.h"
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}  
+
+void processInput(GLFWwindow *window)
+{
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+
+int main(){
+  
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+  
+  GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+  if (window == NULL)
+  {
+    std::cout << "Failed to create GLFW window" << std::endl;
+    glfwTerminate();
+    return -1;
+  }
+  glfwMakeContextCurrent(window);
+
+  if (!gladLoadGL(glfwGetProcAddress))
+  {
+      std::cout << "Failed to initialize GLAD" << std::endl;
+      return -1;
+  }    
+
+
+  glViewport(0, 0, 800, 600);
+
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);  
+
+  while(!glfwWindowShouldClose(window))
+  {
+    processInput(window);
+    glfwSwapBuffers(window);
+    glfwPollEvents();    
+  }
+  glfwTerminate();
+  std::cout << "Window closed...\n";
+  
+  return 0;
+}
