2aef6f22555ffd1ead84edc78f12076d5a1dca8e
Bober <towermanxxl@gmail.com>
2026-09-17 23:04:50 +0200

    got a triangle rendering xD!

--- main.cpp
+++ main.cpp
@@ -2,6 +2,9 @@
 #include <GLFW/glfw3.h>
 
 #include <iostream>
+#include <fstream>
+
+#define uint unsigned int
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
@@ -14,7 +17,74 @@ void processInput(GLFWwindow *window)
         glfwSetWindowShouldClose(window, true);
 }
 
+std::string readShader(std::string filePath) {
 
+  std::ifstream file(filePath);
+
+  if (!file.is_open()){
+    std::cout << "File does not exist! " << filePath << '\n';
+    std::exit(1);
+  }
+
+  std::string shaderContent;
+  std::string line;
+  while (std::getline(file, line)){
+    shaderContent +=  '\n' + line;
+  }
+  std::cout << shaderContent << '\n';
+  return shaderContent;
+};
+
+uint loadShader(std::string filePath, GLenum type){
+  std::string shaderSource = readShader(filePath);
+  const char* shaderSourceCstr = shaderSource.c_str();
+
+  uint shader;
+  shader = glCreateShader(type);
+  glShaderSource(shader, 1, &shaderSourceCstr, NULL);
+  glCompileShader(shader);  
+  // compilation done, check logs
+
+  int  success;
+  char infoLog[512];
+  glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+  if(!success)
+  {
+      glGetShaderInfoLog(shader, 512, NULL, infoLog);
+      std::cout << "ERROR::SHADER::" << filePath << " ::COMPILATION_FAILED\n" << infoLog << std::endl;
+      std::exit(1);
+  }
+
+  // all good
+  return shader;
+}
+
+
+uint loadShaderProgram(){
+  uint shaderProgram;
+  shaderProgram = glCreateProgram();
+  uint vertexShader = loadShader("shaders/vertex.glsl", GL_VERTEX_SHADER);
+  uint fragmentShader = loadShader("shaders/fragment.glsl", GL_FRAGMENT_SHADER);
+
+  glAttachShader(shaderProgram, vertexShader);
+  glAttachShader(shaderProgram, fragmentShader);
+  glLinkProgram(shaderProgram);
+
+  int  success;
+  char infoLog[512];
+  glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+  if(!success) {
+      glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+      std::cout << "ERROR::SHADER_PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+      std::exit(1);
+  }
+  // all good, cleanup
+
+
+  glDeleteShader(vertexShader);
+  glDeleteShader(fragmentShader);  
+  return shaderProgram;
+}
 int main(){
   
   glfwInit();
@@ -43,10 +113,43 @@ int main(){
 
   glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);  
 
+  float vertices[] = {
+      -0.5f, -0.5f, 0.0f,
+      0.5f, -0.5f, 0.0f,
+      0.0f,  0.5f, 0.0f
+  };  
+
+  
+
+  
+  uint shaderProgram = loadShaderProgram();
+  glUseProgram(shaderProgram);
+
+  unsigned int VBO;
+  glGenBuffers(1, &VBO);  
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);  
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  unsigned int VAO;
+  glGenVertexArrays(1, &VAO); 
+  glBindVertexArray(VAO);
+
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+  glEnableVertexAttribArray(0); 
+
+  
   while(!glfwWindowShouldClose(window))
   {
     processInput(window);
     glfwSwapBuffers(window);
+
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glUseProgram(shaderProgram);
+    glBindVertexArray(VAO);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+
     glfwPollEvents();    
   }
   glfwTerminate();
